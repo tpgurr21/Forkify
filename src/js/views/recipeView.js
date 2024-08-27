3eff1c484f6316d5b7f80b0002579617d9e4860d
Thomas Gurr <tpgurr21@gmail.com>
2024-08-27 17:59:59 -0400

    adhered to the Publisher-Subscriber pattern

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -30,6 +30,10 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
 
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
+
   #generateMarkup() {
     console.log(this.#data);
     return `
