738d17dcbe2e2c1c9d310081fceb085cf4450080
Thomas Gurr <tpgurr21@gmail.com>
2024-08-28 20:59:37 -0400

    implementing search results

--- src/js/model.js
+++ src/js/model.js
@@ -3,11 +3,15 @@ import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -27,3 +31,24 @@ export const loadRecipe = async function (id) {
     throw err;
   }
 };
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    console.log(state.search.results);
+  } catch (err) {
+    console.error(`${err} 💥💥💥💥💩`);
+    throw err;
+  }
+};
+loadSearchResults('pizza');
