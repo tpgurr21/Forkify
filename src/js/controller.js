a964564338126f52b4f0a77f05c02759a9170ac8
Thomas Gurr <tpgurr21@gmail.com>
2024-08-31 18:34:55 -0400

    rendering pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationViews from './views/paginationViews.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -39,6 +40,9 @@ const controlSearchResults = async function () {
 
     // 3) Render results
     resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationViews.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
