0d72bfbcc8f0e0a6b2a5b72deb9bd153d4c1424b
Thomas Gurr <tpgurr21@gmail.com>
2024-10-22 10:18:31 -0400

    refactored bookmark and results views

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,4 +1,5 @@
 import View from './View';
+import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class bookmarksView extends View {
@@ -7,32 +8,9 @@ class bookmarksView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-        <a class="preview__link ${
-          result.id === id ? 'preview__link--active' : ''
-        }" href="#${result.id}">
-              <figure class="preview__fig">
-                <img src="${result.image}" alt="Test" />
-              </figure>
-            <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href="${icons}#icon-user"></use>
-                  </svg>
-                </div>
-            </div>
-        </a>
-    </li>
-    `;
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
   }
 }
 
