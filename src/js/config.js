7c07973eace394b41caae122cd64c5c028f584b3
Thomas Gurr <tpgurr21@gmail.com>
2024-10-22 14:51:30 -0400

    implementing user recipe upload

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '1d5975d6-65b0-470e-a3fd-ba85e22698ff';
+export const MODAL_CLOSE_SEC = 2.5;
