2ab723f4ce49f5e651b216d1aec6774bba35cd4d
Thomas Gurr <tpgurr21@gmail.com>
2024-10-23 19:27:24 -0400

    fixed an issue with fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { fracty } from 'fracty';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
